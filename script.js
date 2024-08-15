c492f9d222efa0d08a5f790b6b090e192d3dd5b3
SyeerO7 <EmalHasitha@proton.me>
2024-08-16 05:09:01 +0530

    Add an event listener to change the div's bg-color Add an event listener to change the div's background color on mouseover

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   container.appendChild(div);
 }
+
+container.querySelectorAll("div").forEach(div => {
+    div.addEventListener("mouseover", () => {
+  div.setAttribute("style", "background: black;");
+})
+});
