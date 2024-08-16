d1468a9979d7d1777d64a039a0707b08a62e60a2
SyeerO7 <EmalHasitha@proton.me>
2024-08-16 07:05:50 +0530

    Add a function to get user input

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
-for (let i = 0; i < 256; i++) {
+let userInput = 16;
+
+for (let i = 0; i < userInput * userInput; i++) {
   const div = document.createElement("div");
   container.appendChild(div);
 }
 
-container.querySelectorAll("div").forEach(div => {
-    div.addEventListener("mouseover", () => {
-  div.setAttribute("style", "background: black;");
-})
+container.querySelectorAll("div").forEach((div) => {
+  div.addEventListener("mouseover", () => {
+    div.setAttribute("style", "background: black;");
+  });
+});
+
+
+
+const getInput = () => {
+  let promptInput = prompt("How many squares do you want per side?", "16");
+  while (!/^[0-9]+$/.test(promptInput)) {
+    alert("Please enter a number");
+    promptInput = prompt("How many squares do you want per side?", "16");
+  }
+};
+
+btn.addEventListener("click", () => {
+  getInput();
 });
