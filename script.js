fd7b9a39a26bc6985880958c7b39220bd6d839ba
SyeerO7 <EmalHasitha@proton.me>
2024-08-16 14:49:05 +0530

    Limit user input to a number between 1 and 101
    * Add a function to change the button text content

--- script.js
+++ script.js
@@ -21,9 +21,14 @@ const changeBGColor = () => {
 };
 
 const getUserInput = () => {
-  promptInput = prompt("How many squares do you want per side?", "64");
-  while (!/^[0-9]+$/.test(promptInput)) {
-    alert("Please enter a number");
+  promptInput = prompt("How many squares do you want per side?\n(1-101)", "64");
+
+  while (
+    promptInput <= 1 ||
+    promptInput > 100 ||
+    !/^[0-9]+$/.test(promptInput)
+  ) {
+    alert("Please enter a number between 1 and 101");
     promptInput = prompt("How many squares do you want per side?", "64");
   }
 };
@@ -41,12 +46,17 @@ btn.addEventListener("click", () => {
   genNewGrid();
   adjustNewGrid();
   changeBGColor();
+  changeButtonText();
 });
 
 function removeCurrentGrid() {
   while (container.firstChild) {
     container.firstChild.remove();
   }
+}
+
+function changeButtonText() {
+  btn.textContent = `${userInput}x${userInput}`;
 }
 
 genNewGrid();
