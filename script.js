4b260a49016280d1183bdd073ba3a56d0ce81c15
SyeerO7 <EmalHasitha@proton.me>
2024-08-16 14:08:03 +0530

    Add an event listener to generate a new grid

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
 const container = document.querySelector(".container");
 const btn = document.querySelector("button");
 
-let userInput = 16;
+let userInput = 64;
+let promptInput = "64";
 
-for (let i = 0; i < userInput * userInput; i++) {
-  const div = document.createElement("div");
-  container.appendChild(div);
-}
-
-container.querySelectorAll("div").forEach((div) => {
-  div.addEventListener("mouseover", () => {
-    div.setAttribute("style", "background: black;");
-  });
-});
-
-
-
-const getInput = () => {
-  let promptInput = prompt("How many squares do you want per side?", "16");
-  while (!/^[0-9]+$/.test(promptInput)) {
-    alert("Please enter a number");
-    promptInput = prompt("How many squares do you want per side?", "16");
+const genNewGrid = () => {
+  userInput = parseInt(promptInput, 10);
+  for (let i = 0; i < userInput * userInput; i++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
   }
 };
 
+const changeBGColor = () => {
+  container.querySelectorAll("div").forEach((div) => {
+    div.addEventListener("mouseover", () => {
+      div.style.backgroundColor = "#000";
+    });
+  });
+};
+
+const getUserInput = () => {
+  promptInput = prompt("How many squares do you want per side?", "64");
+  while (!/^[0-9]+$/.test(promptInput)) {
+    alert("Please enter a number");
+    promptInput = prompt("How many squares do you want per side?", "64");
+  }
+};
+
+const adjustNewGrid = () => {
+  const newFlexBasis = 100 / userInput;
+  container.querySelectorAll("div").forEach((div) => {
+    div.style.flexBasis = `${newFlexBasis}%`;
+  });
+};
+
 btn.addEventListener("click", () => {
-  getInput();
+  getUserInput();
+  removeCurrentGrid();
+  genNewGrid();
+  adjustNewGrid();
+  changeBGColor();
 });
+
+function removeCurrentGrid() {
+  while (container.firstChild) {
+    container.firstChild.remove();
+  }
+}
+
+genNewGrid();
+adjustNewGrid();
+changeBGColor();
